349e1ac99d5bb61b4eed109a078abbd0daee0d7a
steskal <steskal@gmail.com>
2024-12-02 22:42:47 -0500

    Puzzle 1 solved, Puzzle 2 solved by brute-force

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -20,7 +20,6 @@ fn main() {
 			if is_report_safe(&report) {
 				safe_counter_one += 1;
 				safe_counter_two += 1;
-				//} else if is_corrected_report_safe(&report) {
 			} else if brute_force(&report) {
 				safe_counter_two += 1;
 			}
@@ -64,61 +63,11 @@ fn is_report_safe(_report: &Vec<i32>) -> bool {
 		}
 		previous = it.clone();
 	}
-	
 	return asc ^ desc
 }
 
-fn is_corrected_report_safe(_report: &Vec<i32>) -> bool {
-	
-	//lets use 0 for out of range (diff == 0 || > 3) , 1 for up adn 2 for down
-	let mut direction: Vec<i8>	= Vec::new();
-	
-	let mut previous: i32		= -1;
-	
-	for it in _report.iter() {
-		if previous != -1 {
-			let diff = it - previous;
-			
-			if diff.abs() >=1 && diff.abs() <=3 {
-				if diff > 0 {
-					direction.push(1);
-				} else {
-					direction.push(2);
-				}
-			} else {
-				direction.push(0);
-			}
-		}
-
-		previous = it.clone();
-	}
-	
-	let oor	= direction.iter().filter(|&&x| x == 0).count();
-	let up	= direction.iter().filter(|&&x| x == 1).count();
-	let dwn = direction.iter().filter(|&&x| x == 2).count();
-	
-	println!("Counts - Oor: {}, up: {}, down: {}, report.len(): {}", oor, up, dwn, _report.len());
-	
-	if (up == _report.len() - 2) || (dwn == _report.len() - 2) {
-		let mut idx;
-		
-		if oor == 1 {
-			idx = direction.iter().position(|&x| x == 0).unwrap();
-		} else if up == 1 {
-			idx = direction.iter().position(|&x| x == 1).unwrap();
-		} else {
-			idx = direction.iter().position(|&x| x == 2).unwrap();
-		}
-		
-		idx += 1; // direction vector is one shor2ter compared to report
-		
-		let mut _reduced_report = _report.clone();
-		_reduced_report.remove(idx);
-		return is_report_safe(&_reduced_report);
-	}
-	false
-}
-
+// This code just iterates through report and removes one element in each pass
+// until the report without it is safe or until it tries all unsuccessfully
 fn brute_force(_report: &Vec<i32>) -> bool {
 	
 	for idx in 0.._report.len() {
@@ -129,6 +78,5 @@ fn brute_force(_report: &Vec<i32>) -> bool {
 			return true
 		}
 	}
-	
 	return false
 }
